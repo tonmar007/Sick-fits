3bb18acba87953b49a71cc341b7842afcd10a3b1
tonmar007 <tonmar005@gmail.com>
2023-07-19 17:35:27 +0200

    React Testing Library

---
+++ frontend/__tests__/Product.test.js
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import { MockedProvider } from '@apollo/react-testing';
+import Product from '../components/Product';
+import { fakeItem } from '../lib/testUtils';
+
+const product = fakeItem();
+
+describe('<Product />', () => {
+  it('renders out the price tag and title', () => {
+    const { container, debug } = render(
+      <MockedProvider>
+        <Product product={product} />
+      </MockedProvider>
+    );
+    const priceTag = screen.getByText('$50');
+    expect(priceTag).toBeInTheDocument();
+    const link = container.querySelector('a');
+    expect(link).toHaveAttribute('href', '/product/abc123');
+    expect(link).toHaveTextContent(product.name);
+  });
+});
