b868b0a29cd16a4875d0f263eba9cc6e1954e2a6
tonmar007 <tonmar005@gmail.com>
2023-08-02 12:44:06 +0200

    Snapshot Testing

--- frontend/__tests__/Product.test.js
+++ frontend/__tests__/Product.test.js
@@ -18,4 +18,13 @@ describe('<Product />', () => {
     expect(link).toHaveAttribute('href', '/product/abc123');
     expect(link).toHaveTextContent(product.name);
   });
+
+  it('Renders and mathces the snapshot', () => {
+    const { container, debug } = render(
+      <MockedProvider>
+        <Product product={product} />
+      </MockedProvider>
+    );
+    expect(container).toMatchSnapshot();
+  });
 });
